0e118836fa5a1f11ec778379b34ccfb5f73a2d1a
bang9878 <wnsduq1225@gmail.com>
2025-10-24 12:18:16 +0900

    init project

---
+++ backend/src/main/java/kr/co/ouroboros/OuroborosApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ouroboros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuroborosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuroborosApplication.class, args);
+	}
+
+}
