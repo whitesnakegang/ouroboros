5e6e55d1dca0975ab6e2f730accfb7c21bb31bae
박서진 <002seojin@gmail.com>
2025-10-28 11:31:20 +0900

    fix: thread 안전하게 faker config  수정

--- backend/src/main/java/kr/co/ouroboros/core/global/config/FakerConfig.java
+++ backend/src/main/java/kr/co/ouroboros/core/global/config/FakerConfig.java
@@ -1,14 +1,15 @@
 package kr.co.ouroboros.core.global.config;
 
 import net.datafaker.Faker;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.Locale;
 
 @Configuration
 public class FakerConfig {
-    @Bean
-    public Faker faker() {
-        return new Faker(Locale.KOREA);
+    private static final ThreadLocal<Faker> threadLocalFaker =
+            ThreadLocal.withInitial(() -> new Faker(Locale.KOREA));
+
+    private Faker faker() {
+        return threadLocalFaker.get();
     }
 }
