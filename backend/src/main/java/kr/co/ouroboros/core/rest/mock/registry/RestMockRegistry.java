7bb95e6174cf336432f01ebf727d0c0193c2f951
박서진 <002seojin@gmail.com>
2025-10-28 11:10:26 +0900

    feature: rest한정 registry 구현

---
+++ backend/src/main/java/kr/co/ouroboros/core/rest/mock/registry/RestMockRegistry.java
@@ -0,0 +1,34 @@
+package kr.co.ouroboros.core.rest.mock.registry;
+
+import kr.co.ouroboros.core.global.mock.registry.MockRegistryBase;
+import kr.co.ouroboros.core.rest.mock.model.EndpointMeta;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RestMockRegistry implements MockRegistryBase<EndpointMeta> {
+
+    private final Map<String, EndpointMeta> registry = new ConcurrentHashMap<>();
+
+    private String key(String path, String method) {
+        return method.toUpperCase() + ":" + path;
+    }
+
+    @Override
+    public void register(EndpointMeta meta) {
+        registry.put(key(meta.getPath(), meta.getMethod()), meta);
+    }
+
+    @Override
+    public Optional<EndpointMeta> find(String path, String method) {
+        return Optional.ofNullable(registry.get(key(path, method)));
+    }
+
+    @Override
+    public void clear() {
+        registry.clear();
+    }
+}
