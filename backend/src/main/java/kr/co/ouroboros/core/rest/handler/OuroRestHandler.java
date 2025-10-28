9da2e4488e3e0256dec1d5a48b389842388db734
wnsduq4258 <wnsduq4258@naver.com>
2025-10-28 18:03:59 +0900

    feat: 역직렬화 로직 구현

--- backend/src/main/java/kr/co/ouroboros/core/rest/handler/OuroRestHandler.java
+++ backend/src/main/java/kr/co/ouroboros/core/rest/handler/OuroRestHandler.java
@@ -1,17 +1,31 @@
 package kr.co.ouroboros.core.rest.handler;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.OpenAPI;
+import java.util.Locale;
+import java.util.Map;
 import kr.co.ouroboros.core.global.Protocol;
 import kr.co.ouroboros.core.global.handler.OuroProtocolHandler;
 import kr.co.ouroboros.core.global.spec.OuroApiSpec;
+import kr.co.ouroboros.core.rest.common.dto.OuroRestApiSpec;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.service.OpenAPIService;
 import org.springframework.stereotype.Component;
+import org.yaml.snakeyaml.Yaml;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class OuroRestHandler implements OuroProtocolHandler {
 
-    private final OpenAPI springDocOpenApi;
+    private final OpenAPIService openAPIService;
+
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true); // DTO에 @JsonIgnoreProperties로 안전
+
 
     /**
      * Identifies this handler's protocol as REST.
@@ -25,17 +39,35 @@ public class OuroRestHandler implements OuroProtocolHandler {
 
     @Override
     public String getSpecFilePath() {
-        return "";
+        return "/ouroboros/rest/ourorest.yml";
     }
 
     @Override
     public OuroApiSpec scanCurrentState() {
-        return null;
+        try {
+            OpenAPI model = openAPIService.getCachedOpenAPI(Locale.KOREA);
+
+            log.info("OpenAPI model : {}", model);
+
+            String json = Json.mapper().writeValueAsString(model);
+            OuroRestApiSpec spec = mapper.readValue(json, OuroRestApiSpec.class);
+
+            if (spec.getInfo() != null && spec.getInfo().getVersion() == null) {
+                spec.getInfo().setVersion("v1");
+            }
+
+            return spec;
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to scan current OpenAPI state", e);
+        }
     }
 
     @Override
     public OuroApiSpec loadFromFile(String yamlContent) {
-        return null;
+        Yaml yaml = new Yaml();
+        Map<String, Object> map = yaml.load(yamlContent);
+
+        return mapper.convertValue(map, OuroRestApiSpec.class);
     }
 
     @Override
