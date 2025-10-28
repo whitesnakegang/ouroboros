80a7b4ce0f3c0b29de9829c2ad480c185258eb8d
wnsduq4258 <wnsduq4258@naver.com>
2025-10-28 18:03:05 +0900

    refactor: OuroApiSpec 구현체로 변경

--- backend/src/main/java/kr/co/ouroboros/core/rest/common/dto/OuroRestApiSpec.java
+++ backend/src/main/java/kr/co/ouroboros/core/rest/common/dto/OuroRestApiSpec.java
@@ -3,13 +3,14 @@ package kr.co.ouroboros.core.rest.common.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 import java.util.Map;
+import kr.co.ouroboros.core.global.spec.OuroApiSpec;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class OuroRestApiSpec {
+public class OuroRestApiSpec implements OuroApiSpec {
 
     private String openapi;
     private Info info;
@@ -17,4 +18,14 @@ public class OuroRestApiSpec {
     private Components components;
     private List<Server> servers;
     private List<Map<String, List<String>>> security;
+
+    @Override
+    public String getProtocol() {
+        return "rest";
+    }
+
+    @Override
+    public String getVersion() {
+        return info != null ? info.getVersion() : "";
+    }
 }
