93c2e4db7734ac4ae506107e34d20d8c6e5f30ec
신진우 <sjw0066@naver.com>
2025-11-05 13:38:06 +0900

    fix: 프론트 포함 배포를위한 수정

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -8,7 +8,7 @@ import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/ouroboros' : '/'}>
       <AppProvider>
         <Routes>
           <Route path="/" element={<RootLayout />}>
