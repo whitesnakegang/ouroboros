88dd68e7c120a45771f854be11f009b7487e0ab9
wnsduq4258 <wnsduq4258@naver.com>
2025-11-12 21:31:41 +0900

    refactor: ChatMessage 구조 변경

--- backend/src/main/java/kr/co/ouroboros/core/websocket/test/ChatMessage.java
+++ backend/src/main/java/kr/co/ouroboros/core/websocket/test/ChatMessage.java
@@ -1,5 +1,13 @@
 package kr.co.ouroboros.core.websocket.test;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     public enum Type { ENTER, CHAT, LEAVE }
 
@@ -7,22 +15,6 @@ public class ChatMessage {
     private String roomId;   // 간단히 방 개념도 넣어둠 (원하면 고정 사용 가능)
     private String sender;
     private String content;
-
-    public ChatMessage() {}
-
-    public ChatMessage(Type type, String roomId, String sender, String content) {
-        this.type = type;
-        this.roomId = roomId;
-        this.sender = sender;
-        this.content = content;
-    }
-
-    public Type getType() { return type; }
-    public void setType(Type type) { this.type = type; }
-    public String getRoomId() { return roomId; }
-    public void setRoomId(String roomId) { this.roomId = roomId; }
-    public String getSender() { return sender; }
-    public void setSender(String sender) { this.sender = sender; }
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
+    private User user;
+    private List<String> list;
 }
