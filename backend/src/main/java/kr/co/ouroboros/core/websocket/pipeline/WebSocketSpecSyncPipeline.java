fc710ef3b1054dc509db997085d7528859d96217
Kangboom <jkl6147@naver.com>
2025-11-13 01:30:45 +0900

    fix: 채널명 파싱 문제 해결

--- backend/src/main/java/kr/co/ouroboros/core/websocket/pipeline/WebSocketSpecSyncPipeline.java
+++ backend/src/main/java/kr/co/ouroboros/core/websocket/pipeline/WebSocketSpecSyncPipeline.java
@@ -41,18 +41,18 @@ public class WebSocketSpecSyncPipeline implements SpecSyncPipeline {
         for(String operationKey :  scanOpMap.keySet()) {
 
             Operation scanOp = scanOpMap.get(operationKey); // TODO: 나중에 Tags 비교 및 Schema 비교 로직에서 사용 예정
-            String channelName = WebSocketSpecSyncHelper.extractChannelNameFromOperationKey(operationKey);
+            String channelRef = scanOp.getChannel().getRef();
 
             // TODO : scanOp에서 progress == null 건너 뜀
             if(scanOp.getXOuroborosProgress() == null) continue;
             
             // 파일에 없는 경우 추가
-            if(!fileChannelNameOperationMap.containsKey(channelName)){
+            if(!fileChannelNameOperationMap.containsKey(channelRef)){
                 scanOp.setXOuroborosDiff("channel");
                 fileOpMap.put(operationKey, scanOp);
                 
                 // Operation이 참조하는 Channel 추가
-                WebSocketSpecSyncHelper.addReferencedChannel(wsFileSpec, wsScannedSpec, scanOp, channelName);
+                WebSocketSpecSyncHelper.addReferencedChannel(wsFileSpec, wsScannedSpec, channelRef);
                 
                 // Operation이 참조하는 Message와 Schema 추가
                 WebSocketSpecSyncHelper.addReferencedMessagesAndSchemas(wsFileSpec, wsScannedSpec, scanOp);
@@ -62,14 +62,14 @@ public class WebSocketSpecSyncPipeline implements SpecSyncPipeline {
             
             // TODO Tags 비교해서 x-ouroboros-progress 업데이트 [AUTHOR : 임강범]
             if(!scanOp.getXOuroborosProgress().equals("completed")) {
-                for (Operation operation : fileChannelNameOperationMap.get(channelName)) {
+                for (Operation operation : fileChannelNameOperationMap.get(channelRef)) {
                     operation.setXOuroborosProgress(scanOp.getXOuroborosProgress());
                 }
                 continue;
             }
 
             // TODO Schema 정답지 비교 COMPLETED인 경우만 여기 로직 실행 [AUTHOR : 임강범]
-            Operation fileOp = fileChannelNameOperationMap.get(operationKey)
+            Operation fileOp = fileChannelNameOperationMap.get(channelRef)
                     .get(0);
             MessageReference fileMessage = fileOp.getMessages().get(0);
             MessageReference scanMessage = scanOp.getMessages().get(0);
@@ -78,18 +78,18 @@ public class WebSocketSpecSyncPipeline implements SpecSyncPipeline {
                 // $ref가 같은 경우 schema 정답지 확인
                 if(schemaMap.get(WebSocketSpecSyncHelper.extractClassNameFromRef(scanMessage.getRef()))){
                     // 정답인 경우
-                    for (Operation operation : fileChannelNameOperationMap.get(channelName)) {
+                    for (Operation operation : fileChannelNameOperationMap.get(channelRef)) {
                         operation.setXOuroborosProgress("completed");
                         operation.setXOuroborosDiff("none");
                     }
                 } else {
-                    for (Operation operation : fileChannelNameOperationMap.get(channelName)) {
+                    for (Operation operation : fileChannelNameOperationMap.get(channelRef)) {
                         operation.setXOuroborosProgress("none");
                         operation.setXOuroborosDiff("message");
                     }
                 }
             } else {
-                for (Operation operation : fileChannelNameOperationMap.get(channelName)) {
+                for (Operation operation : fileChannelNameOperationMap.get(channelRef)) {
                     operation.setXOuroborosProgress("none");
                     operation.setXOuroborosDiff("message");
                 }
