6d667ad58c84d48d9f454fddf28273332baf81c8
wnsduq4258 <wnsduq4258@naver.com>
2025-11-12 14:42:13 +0900

    refactor: 커스텀 필드 tag 사용안하는 방식으로 수정

--- backend/src/main/java/kr/co/ouroboros/core/websocket/common/dto/Operation.java
+++ backend/src/main/java/kr/co/ouroboros/core/websocket/common/dto/Operation.java
@@ -1,6 +1,8 @@
 package kr.co.ouroboros.core.websocket.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.Map;
 import lombok.Data;
@@ -16,13 +18,26 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        isGetterVisibility = JsonAutoDetect.Visibility.NONE
+)
 public class Operation {
 
     private String action;
     private ChannelReference channel;
-    private List<Map<String, String>> tags;
     private Map<String, Object> bindings;
     private List<MessageReference> messages;
     private Reply reply;
+
+    @JsonProperty("x-ouroboros-id")
+    private String xOuroborosId;
+
+    @JsonProperty("x-ouroboros-progress")
+    private String xOuroborosProgress;
+
+    @JsonProperty("x-ouroboros-diff")
+    private String xOuroborosDiff;
 }
 
